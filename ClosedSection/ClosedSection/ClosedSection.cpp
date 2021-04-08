9f0ef5d8ebbe83a4f97f139f3d88155b358b8030
Desire4sorrow <56004655+Desire4sorrow@users.noreply.github.com>
2021-04-08 23:05:00 +0300

    added 5th lab

---
+++ ClosedSection/ClosedSection/ClosedSection.cpp
@@ -0,0 +1,54 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+
+CRITICAL_SECTION CriticalSection;
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+	//EnterCriticalSection(&CriticalSection);
+
+	int* working_variable = (int*)lpParam;
+
+	for (int i = 0; i < 50; i++) {
+		int j = *working_variable;
+		*working_variable = j + 1;
+	}
+
+	//добавить вывод в консоль значений двух переменных для последующего сравнения на ошибку
+
+	//LeaveCriticalSection(&CriticalSection);
+
+	ExitThread(0);
+}
+
+
+int main(int argc)
+{
+	int a;
+	std::cin >> a;
+	int working_variable = 0;
+
+	if (!InitializeCriticalSectionAndSpinCount(&CriticalSection,
+		0x00000400))
+		return 0;
+
+	HANDLE* handles = new HANDLE[2];
+
+	for (int i = 0; i < 2; i++) {
+		handles[i] = CreateThread(NULL, i, &ThreadProc, &working_variable, CREATE_SUSPENDED, NULL);
+		SetThreadAffinityMask(handles[i], 1 << 4);
+	}
+
+	for (int i = 0; i < 2; i++) {
+		ResumeThread(handles[i]);
+	}
+
+	WaitForMultipleObjects(2, handles, true, INFINITE);
+
+	std::cout << working_variable << std::endl;
+
+	//DeleteCriticalSection(&CriticalSection);
+
+	return 0;
+}
